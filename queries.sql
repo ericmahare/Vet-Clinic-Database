523043af73918e89a91b11bbbb43bd183c807e2e
ericmahare <erikmahare@gmail.com>
2022-07-05 13:26:12 +0300

    Fetch data queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT * FROM animals WHERE neutered=TRUE and escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name='Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
