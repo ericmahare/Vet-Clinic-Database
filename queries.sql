dd8c9279ac8d67b63c0ffc5c5a3ec25cb78b366a
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-07-12 15:04:57 +0100

    add EXOAIN ANALYSE query

--- queries.sql
+++ queries.sql
@@ -230,3 +230,9 @@ WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY total_visits DESC
 LIMIT 1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
